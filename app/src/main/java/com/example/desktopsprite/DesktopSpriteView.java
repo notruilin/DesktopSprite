1a7aad9294af762946f97b37a6e32535ec7a72fb
ruilin <notruilin@gmail.com>
2019-10-06 04:34:30 +1100

    Add Hang anim, dialog

--- app/src/main/java/com/example/desktopsprite/DesktopSpriteView.java
+++ app/src/main/java/com/example/desktopsprite/DesktopSpriteView.java
@@ -1,14 +1,20 @@
 package com.example.desktopsprite;
 
-import android.app.Activity;
 import android.content.Context;
+import android.graphics.drawable.AnimationDrawable;
 import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class DesktopSpriteView extends FrameLayout {
     public int spriteWidth, spriteHeight;
@@ -16,14 +22,19 @@ public class DesktopSpriteView extends FrameLayout {
 
     private int spriteX, spriteY;
     private WindowManager.LayoutParams spriteParams;
+    private AnimationDrawable animationDrawable;
 
+    private Context context;
     private final WindowManager windowManager;
 
     public DesktopSpriteView(Context context) {
         super(context);
+        this.context = context;
         windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
         LayoutInflater.from(context).inflate(R.layout.sprite_layout, this);
-        FrameLayout view = findViewById(R.id.sprite_layout);
+        ImageView imageView = findViewById(R.id.sprite);
+        imageView.setImageResource(R.drawable.stand);
+        RelativeLayout view = findViewById(R.id.sprite_layout);
         spriteWidth = view.getLayoutParams().width;
         spriteHeight = view.getLayoutParams().height;
         DisplayMetrics metrics = new DisplayMetrics();
@@ -37,6 +48,8 @@ public class DesktopSpriteView extends FrameLayout {
         //Log.w("myApp", "touched!");
         switch (event.getActionMasked()) {
             case MotionEvent.ACTION_DOWN:
+                playHangAnim();
+                showDialog("Put me down!!!!", 1000);
                 spriteX = (int) event.getRawX();
                 spriteY = (int) event.getRawY();
                 //Log.w("myApp", "down! " + "X: " + ((Integer)spriteX).toString() + " Y: " + ((Integer)spriteY).toString());
@@ -51,6 +64,8 @@ public class DesktopSpriteView extends FrameLayout {
                     updateSpritePosition(dx, dy);
                 }
                 break;
+            case MotionEvent.ACTION_UP:
+                stopHangAnim();
         }
         return true;
     }
@@ -63,5 +78,52 @@ public class DesktopSpriteView extends FrameLayout {
         spriteParams.x += dx;
         spriteParams.y += dy;
         windowManager.updateViewLayout(this, spriteParams);
+    }
+
+    void playHangAnim() {
+        ImageView imageView = findViewById(R.id.sprite);
+        imageView.setImageResource(R.drawable.hang_anim);
+        animationDrawable = (AnimationDrawable) imageView.getDrawable();
+        animationDrawable.start();
+    }
+
+    void stopHangAnim() {
+        animationDrawable.stop();
+        ImageView imageView = findViewById(R.id.sprite);
+        imageView.setImageResource(R.drawable.stand);
+    }
+
+    void showDialog(String txt, int duration) {
+        View dialogLayout = findViewById(R.id.dialog_layout);
+        if (dialogLayout == null) {
+            LayoutInflater inflater = LayoutInflater.from(context);
+            View spriteLayout = inflater.inflate(R.layout.sprite_layout, null);
+            dialogLayout = inflater.inflate(R.layout.dialog_layout, (ViewGroup) spriteLayout, false);
+            this.addView(dialogLayout);
+        }
+        else {
+            dialogLayout.setVisibility(View.VISIBLE);
+        }
+
+        TextView textView = findViewById(R.id.dialog_txt);
+        textView.setText(txt);
+
+        if (spriteX < screenWidth / 2) {
+            findViewById(R.id.dialog_left_triangle).setVisibility(View.VISIBLE);
+            findViewById(R.id.dialog_right_triangle).setVisibility(View.GONE);
+        }
+        else {
+            findViewById(R.id.dialog_right_triangle).setVisibility(View.VISIBLE);
+            findViewById(R.id.dialog_left_triangle).setVisibility(View.GONE);
+        }
+
+        dialogLayout.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                findViewById(R.id.dialog_layout).setVisibility(View.GONE);
+            }
+        }, duration);
+
+        findViewById(R.id.sprite_layout).forceLayout();
     }
 }
