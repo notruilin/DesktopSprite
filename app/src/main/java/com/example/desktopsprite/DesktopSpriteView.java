a2ec9d5be63a1dabed805a77a8e9c7653de32735
Hina <410023212@qq.com>
2019-10-09 15:59:56 +0800

    picture_see

--- app/src/main/java/com/example/desktopsprite/DesktopSpriteView.java
+++ app/src/main/java/com/example/desktopsprite/DesktopSpriteView.java
@@ -41,7 +41,7 @@ public class DesktopSpriteView extends LinearLayout {
         windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
         LayoutInflater.from(context).inflate(R.layout.sprite_layout, this);
         imageView = findViewById(R.id.sprite);
-        imageView.setImageResource(R.drawable.sleep_play_aeolian_bell);
+        imageView.setImageResource(R.drawable.shower);
         animationDrawable = (AnimationDrawable) imageView.getDrawable();
         animationDrawable.start();
 
